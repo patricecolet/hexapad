a72b942fd0a13fba3b5acc23eb05ecfdfee5eee9
guest <jchrstphjoseph@gmail.com>
2024-03-22 14:33:01 +0100

    maj mode boutton

--- midimessage.cpp
+++ midimessage.cpp
@@ -4,27 +4,27 @@ midiMessage::midiMessage()  {
 };
 
 void midiMessage::sendNoteOn(PadSettings pad, uint8_t velocity) {
-    /* 
-  uint8_t velo = 0
+ 
+  uint8_t velo = 0;
   if(pad.velocity_curve == curveType::linear) {
-    //velo = 
+    velo = velocity;
   }
   else if(pad.velocity_curve == curveType::parabola) {
-
+    velo = ((127*(velocity)^2)/127^2);
   }
   else if(pad.velocity_curve == curveType::hyperbola) {
-
+    velo = round(127*(1-exp(-1.5*velocity/40)));
   }
   else if(pad.velocity_curve == curveType::sigmoid) {
-
-  }*/
+    velo = round(127/(1+exp(-0.08*(velocity-65))));
+  }
           Serial.print("Note on Note: \n");
           Serial.println(pad.note);
           /*
           Serial.print("Channel On: \n");
           Serial.println(pad.channel);*/
       //midiEventPacket_t event = {0x09, 0x90 | _address.channel, _address.address, velocity};
-      midiEventPacket_t event = {0x09, 0x90 | pad.channel, pad.note, velocity};
+      midiEventPacket_t event = {0x09, 0x90 | pad.channel, pad.note, velo};
       MidiUSB.sendMIDI(event);
 };
 
